f0d47b61e3dfbd6c0563d121f0dff38580638537
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-11-27 17:47:33 -0500

    Se ajusta proxy

--- proxy.prod.conf.js
+++ proxy.prod.conf.js
@@ -1,63 +1,63 @@
 module.exports = {
   "/api/v1/usuario/*": {
-      "target": "http://sistema-sah-usuarios-e2efdyb8c6fva2ax.mexicocentral-01.azurewebsites.net/api/v1/usuario/",
-      "secure": false,
+      "target": "https://sistema-sah-usuarios-e2efdyb8c6fva2ax.mexicocentral-01.azurewebsites.net/api/v1/usuario/",
+      "secure": true,
       "logLevel": "debug",
       "pathRewrite": {
         "^/api/v1/usuario/": ""
       }
   },
   "/api/v1/tipo-usuario/*": {
-      "target": "http://sistema-sah-usuarios-e2efdyb8c6fva2ax.mexicocentral-01.azurewebsites.net/api/v1/tipo-usuario/",
-      "secure": false,
+      "target": "https://sistema-sah-usuarios-e2efdyb8c6fva2ax.mexicocentral-01.azurewebsites.net/api/v1/tipo-usuario/",
+      "secure": true,
       "logLevel": "debug",
       "pathRewrite": {
         "^/api/v1/tipo-usuario/": ""
       }
   },
   "/api/v1/tipo-cuarto/*": {
-    "target": "http://sistema-sah-reservas-ms-cngzf4d0aacsbehr.mexicocentral-01.azurewebsites.net/api/v1/tipo-cuarto/",
-    "secure": false,
+    "target": "https://sistema-sah-reservas-ms-cngzf4d0aacsbehr.mexicocentral-01.azurewebsites.net/api/v1/tipo-cuarto/",
+    "secure": true,
     "logLevel": "debug",
     "pathRewrite": {
       "^/api/v1/tipo-cuarto/": ""
     }
   },
   "/api/v1/reserva/*": {
-    "target": "http://sistema-sah-reservas-ms-cngzf4d0aacsbehr.mexicocentral-01.azurewebsites.net/api/v1/reserva/",
-    "secure": false,
+    "target": "https://sistema-sah-reservas-ms-cngzf4d0aacsbehr.mexicocentral-01.azurewebsites.net/api/v1/reserva/",
+    "secure": true,
     "logLevel": "debug",
     "pathRewrite": {
       "^/api/v1/reserva/": ""
     }
   },
   "/api/v1/facturacion/*": {
-    "target": "http://sistema-sah-reservas-ms-cngzf4d0aacsbehr.mexicocentral-01.azurewebsites.net/api/v1/facturacion/",
-    "secure": false,
+    "target": "https://sistema-sah-reservas-ms-cngzf4d0aacsbehr.mexicocentral-01.azurewebsites.net/api/v1/facturacion/",
+    "secure": true,
     "logLevel": "debug",
     "pathRewrite": {
       "^/api/v1/facturacion/": ""
     }
   },          
   "/api/v1/cuartos/*": {
-    "target": "http://sistema-sah-servicios-g7hxe3dwbzhkdnfp.mexicocentral-01.azurewebsites.net/api/v1/cuartos/",
-    "secure": false,
+    "target": "https://sistema-sah-servicios-g7hxe3dwbzhkdnfp.mexicocentral-01.azurewebsites.net/api/v1/cuartos/",
+    "secure": true,
     "logLevel": "debug",
     "pathRewrite": {
       "^/api/v1/cuartos/": ""
     }
   },         
   "/api/v1/servicios/*": {
-    "target": "http://sistema-sah-servicios-g7hxe3dwbzhkdnfp.mexicocentral-01.azurewebsites.net/api/v1/servicios/",
-    "secure": false,
+    "target": "https://sistema-sah-servicios-g7hxe3dwbzhkdnfp.mexicocentral-01.azurewebsites.net/api/v1/servicios/",
+    "secure": true,
     "logLevel": "debug",
     "pathRewrite": {
       "^/api/v1/servicios/": ""
     }
   },         
   "/api/v1/alimentos/*": {
-    "target": "http://sistema-sah-servicios-g7hxe3dwbzhkdnfp.mexicocentral-01.azurewebsites.net/api/v1/alimentos/",
-    "secure": false,
+    "target": "https://sistema-sah-servicios-g7hxe3dwbzhkdnfp.mexicocentral-01.azurewebsites.net/api/v1/alimentos/",
+    "secure": true,
     "logLevel": "debug",
     "pathRewrite": {
       "^/api/v1/alimentos/": ""
