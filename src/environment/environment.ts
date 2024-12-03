e058a4663063d941b1391abe8204fe28f2538c68
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-12-02 21:47:46 -0500

    Se ajusta archivo de despliegue

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
     repoImg: {
       urlBase: 'https://imagenesmf.blob.core.windows.net?',
       name: 'imagenes',
-      sasToken: 'sp=racwdl&st=2024-09-27T20:50:57Z&se=2025-12-12T04:50:57Z&sip=201.190.119.66&sv=2022-11-02&sr=c&sig=CzrRkCOCRU6G2ZLEz3Ev0kwUxx137H4MWOr1JX7K5zU%3D'
+      sasToken: 'sp=racwdli&st=2024-12-03T01:40:50Z&se=2024-12-18T09:40:50Z&sip=201.190.119.66&sv=2022-11-02&sr=c&sig=5kO89wlfItl6MMZ7It0al4fBYyJN7TMfobUyTsKZEKc%3D'
     },
     file:{
       charactersValidation: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
