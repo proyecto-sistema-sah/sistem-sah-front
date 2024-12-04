1fbe2422fd102a598851e218b67fa93811a6eb0b
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-12-03 20:18:08 -0500

    Se ajusta blob storage

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
     repoImg: {
       urlBase: 'https://imagenesmf.blob.core.windows.net?',
       name: 'imagenes',
-      sasToken: 'sp=rcwd&st=2024-12-03T04:36:19Z&se=2024-12-03T12:36:19Z&sip=201.190.119.66-158.23.105.0&sv=2022-11-02&sr=c&sig=p0uhVBc3S1qijZAS4WNwwthsclZ8FDsC65vkmDm07%2B8%3D'
+      sasToken: 'sp=rcwd&st=2024-12-02T01:03:26Z&se=2024-12-31T09:03:26Z&sip=201.190.119.66&sv=2022-11-02&sr=c&sig=blJtoCFTBIJZk640ZqvCK3m2lAknad08puguUTGIrUc%3D'
     },
     file:{
       charactersValidation: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
